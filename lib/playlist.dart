b1268091e7a8cd5e537e6fab40bec8244a399631
Taylor Nelms <taylornelms15@gmail.com>
2023-07-23 16:21:55 -0400

    Playlist basic implementation, SoundSample fixes
    Signed-off-by: Taylor Nelms <taylornelms15@gmail.com>

---
+++ lib/playlist.dart
@@ -0,0 +1,40 @@
+import "soundsample.dart";
+import 'package:xml/xml.dart' as xml;
+
+typedef SoundSampleList = List<SoundSample>;
+
+/// @brief Class representing a playable collection of sound samples
+///
+/// This class represents everything that can be set by a user for
+/// designing a collection of sounds to later play back.
+/// It is not meant to be reflective of any current playback status.
+class Playlist
+{
+  // Constructors
+  Playlist()
+    : name = ""
+    , sampleList = <SoundSample>[]
+  ;
+
+  Playlist.fromXmlElement(xml.XmlElement element)
+    : name = element.getAttribute("title")!
+    , sampleList = <SoundSample>[]
+  {
+    assert(element.name.toString() == Playlist.elementName);
+    for(final childElement in element.childElements) {
+      if (childElement.name.toString() == SoundSample.elementName) {
+        sampleList.add(SoundSample.fromXmlElement(childElement));
+      }
+    }
+  }
+
+  // Static members
+  static const String elementName = "Playlist";
+
+  // Public members
+  String name;
+  double volumeFactor = 1.0;
+  SoundSampleList sampleList;
+
+  // Private members
+}
--- lib/soundsample.dart
+++ lib/soundsample.dart
@@ -8,21 +8,25 @@ import 'package:xml/xml.dart' as xml;
 class SoundSample
 {
   // Constructors
-  SoundSample() :
-      name = "New SoundSample"
+  SoundSample()
+    : name = "New SoundSample"
   ;
 
-  SoundSample.fromXmlElement(xml.XmlElement element) :
-    name = element.getAttribute("title")!
+  SoundSample.fromXmlElement(xml.XmlElement element)
+    : name = element.getAttribute("title")!
     , _resourceUrl = element.getAttribute("resource_url")
     , startTimestamp = double.tryParse(element.getAttribute("start_timestamp") ?? "0.0") ?? 0.0
     , endTimestamp = double.tryParse(element.getAttribute("end_timestamp") ?? "0.0") ?? 0.0
     , volumeFactor = double.tryParse(element.getAttribute("volume_factor") ?? "0.0") ?? 0.0
   {
+    assert(element.name.toString() == SoundSample.elementName);
     if (_resourceUrl != null) {
       //TODO: get time data from the resource_url
     }
   }
+
+  // Static members
+  static const String elementName = "SoundSample";
 
   // Public members
   String name;
