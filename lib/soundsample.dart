bb86b391af7f58726e3f8d9979585c6c7211b7b1
Taylor Nelms <taylornelms15@gmail.com>
2023-07-23 15:59:45 -0400

    SoundSample basic implementation
    Signed-off-by: Taylor Nelms <taylornelms15@gmail.com>

--- lib/soundsample.dart
+++ lib/soundsample.dart
@@ -1,3 +1,5 @@
+import 'package:xml/xml.dart' as xml;
+
 /// @brief Class for storing records of playable audio
 ///
 /// The SoundSample should record everything that can be set by a user
@@ -5,14 +7,31 @@
 /// of any current playback status on its own.
 class SoundSample
 {
+  // Constructors
+  SoundSample() :
+      name = "New SoundSample"
+  ;
+
+  SoundSample.fromXmlElement(xml.XmlElement element) :
+    name = element.getAttribute("title")!
+    , _resourceUrl = element.getAttribute("resource_url")
+    , startTimestamp = double.tryParse(element.getAttribute("start_timestamp") ?? "0.0") ?? 0.0
+    , endTimestamp = double.tryParse(element.getAttribute("end_timestamp") ?? "0.0") ?? 0.0
+    , volumeFactor = double.tryParse(element.getAttribute("volume_factor") ?? "0.0") ?? 0.0
+  {
+    if (_resourceUrl != null) {
+      //TODO: get time data from the resource_url
+    }
+  }
+
   // Public members
-  String name = "";
-  double start_timestamp = 0.0;
-  double end_timestamp = 0.0;
-  double volume_factor = 1.0;
+  String name;
+  double startTimestamp = 0.0;
+  double endTimestamp = 0.0;
+  double volumeFactor = 1.0;
 
   // Private members
-  String? _resource_url;
-  double _total_time = 0.0;
-  double get _total_playable_time => end_timestamp = start_timestamp;
+  String? _resourceUrl;
+  double _totalTime = 0.0;
+  double get _totalPlayableTime => endTimestamp = startTimestamp;
 }
